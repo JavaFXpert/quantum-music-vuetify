8a26bb6f5bfbbd8e032e1875a81834d23de9b1d2
JavaFXpert <james.l.weaver@gmail.com>
2018-02-18 07:34:54 -0500

    Componentizing the Piano

--- grid.js
+++ grid.js
@@ -1,14 +1,3 @@
-// From Piano Project
-var soundpack=[];
-var soundpack_index=[1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,8,8.5,9,9.5,10,11,11.5,12,12.5,13,13.5,14,15];
-for(var i=0;i<soundpack_index.length;i++){
-  soundpack.push({
-    number: soundpack_index[i],
-    url: "https://awiclass.monoame.com/pianosound/set/"+ soundpack_index[i]+".wav"
-  });
-
-}
-
 // The rotation angles to observe
 var rotationangles  = [
   { label: 'CD', value: 0 },
--- piano.js
+++ piano.js
@@ -1,5 +1,4 @@
 
-/*
 var soundpack=[];
 var soundpack_index=[1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,8,8.5,9,9.5,10,11,11.5,12,12.5,13,13.5,14,15];
 for(var i=0;i<soundpack_index.length;i++){
@@ -9,49 +8,58 @@ for(var i=0;i<soundpack_index.length;i++){
   });
 
 }
-*/
 
-/*
-var vm = new Vue({
-  el: "#piano",
-  data: {
-    sounddata: soundpack,
-    notes: [{"num":1,"time":150},{"num":1,"time":265},{"num":5,"time":380},{"num":5,"time":501},{"num":6,"time":625},{"num":6,"time":748},{"num":5,"time":871},{"num":4,"time":1126},{"num":4,"time":1247},{"num":3,"time":1365},{"num":3,"time":1477},{"num":2,"time":1597},{"num":2,"time":1714},{"num":1,"time":1837}],
-    now_note_id: 0,
-    next_note_id: 0,
-    playing_time: 0,
-    record_time: 0,
-    now_press_key: -1,
-    player: null,
-    recorder: null,
-    display_keys:[
-      {num: 1,key: 90  ,type:'white'},
-      {num: 1.5,key: 83  ,type:'black'},
-      {num: 2,key: 88  ,type:'white'},
-      {num: 2.5,key: 68  ,type:'black'},
-      {num: 3,key: 67  ,type:'white'},
-      {num: 4,key: 86  ,type:'white'},
-      {num: 4.5,key: 71  ,type:'black'},
-      {num: 5,key: 66  ,type:'white'},
-      {num: 5.5,key: 72  ,type:'black'},
-      {num: 6,key: 78  ,type:'white'},
-      {num: 6.5,key: 74  ,type:'black'},
-      {num: 7,key: 77  ,type:'white'},
-      {num: 8,key: 81  ,type:'white'},
-      {num: 8.5,key: 50  ,type:'black'},
-      {num: 9,key: 87  ,type:'white'},
-      {num: 9.5,key: 51,type:'black'},
-      {num: 10,key: 69  ,type:'white'},
-      {num: 11,key: 82  ,type:'white'},
-      {num: 11.5,key: 53  ,type:'black'},
-      {num: 12,key: 84  ,type:'white'},
-      {num: 12.5,key: 54  ,type:'black'},
-      {num: 13,key: 89  ,type:'white'},
-      {num: 13.5,key: 55  ,type:'black'},
-      {num: 14,key: 85  ,type:'white'},
-      {num: 15,key: 73  ,type:'white'}
-
-    ]
+var vm = Vue.component('piano-component', {
+  template: '<h1>My Piano</h1>',
+  //el: "#piano",
+  data: function () {
+    return {
+      sounddata: soundpack,
+      notes: [{"num": 1, "time": 150}, {"num": 1, "time": 265}, {"num": 5, "time": 380}, {
+        "num": 5,
+        "time": 501
+      }, {"num": 6, "time": 625}, {"num": 6, "time": 748}, {"num": 5, "time": 871}, {"num": 4, "time": 1126}, {
+        "num": 4,
+        "time": 1247
+      }, {"num": 3, "time": 1365}, {"num": 3, "time": 1477}, {"num": 2, "time": 1597}, {
+        "num": 2,
+        "time": 1714
+      }, {"num": 1, "time": 1837}],
+      now_note_id: 0,
+      next_note_id: 0,
+      playing_time: 0,
+      record_time: 0,
+      now_press_key: -1,
+      player: null,
+      recorder: null,
+      display_keys: [
+        {num: 1, key: 90, type: 'white'},
+        {num: 1.5, key: 83, type: 'black'},
+        {num: 2, key: 88, type: 'white'},
+        {num: 2.5, key: 68, type: 'black'},
+        {num: 3, key: 67, type: 'white'},
+        {num: 4, key: 86, type: 'white'},
+        {num: 4.5, key: 71, type: 'black'},
+        {num: 5, key: 66, type: 'white'},
+        {num: 5.5, key: 72, type: 'black'},
+        {num: 6, key: 78, type: 'white'},
+        {num: 6.5, key: 74, type: 'black'},
+        {num: 7, key: 77, type: 'white'},
+        {num: 8, key: 81, type: 'white'},
+        {num: 8.5, key: 50, type: 'black'},
+        {num: 9, key: 87, type: 'white'},
+        {num: 9.5, key: 51, type: 'black'},
+        {num: 10, key: 69, type: 'white'},
+        {num: 11, key: 82, type: 'white'},
+        {num: 11.5, key: 53, type: 'black'},
+        {num: 12, key: 84, type: 'white'},
+        {num: 12.5, key: 54, type: 'black'},
+        {num: 13, key: 89, type: 'white'},
+        {num: 13.5, key: 55, type: 'black'},
+        {num: 14, key: 85, type: 'white'},
+        {num: 15, key: 73, type: 'white'}
+      ]
+    }
   },
   methods: {
     playnote: function(id,volume){
@@ -130,5 +138,4 @@ var vm = new Vue({
     }
   }
 });
-*/
 
