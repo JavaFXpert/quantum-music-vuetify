a6e29202a69b941b91973a2bc11b32ed3debf5c9
JavaFXpert <james.l.weaver@gmail.com>
2018-02-21 23:26:21 -0500

    Remove unnecessary Piano code

--- piano.js
+++ piano.js
@@ -51,9 +51,6 @@ var vm = Vue.component('piano-component', {
     '</div>',
   data: function () {
     return {
-      soundpack: [],
-      soundpack_index: [1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,8,8.5,9,9.5,10,11,11.5,12,12.5,13,13.5,14,15],
-
       sounddata: soundpack,
       notes: [{"num": 1, "time": 150}, {"num": 1, "time": 265}, {"num": 5, "time": 380}, {
         "num": 5,
